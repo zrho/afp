b12036d26dd1677793c085b8aa8d8b91ff0f299c
Lukas Heidemann <lukasheidemann@gmail.com>
2013-11-04 20:47:32 +0100

    Patched example project to use diagrams 0.7 API.

--- tictactoe/Handler/Circle.hs
+++ tictactoe/Handler/Circle.hs
@@ -5,9 +5,11 @@ import Import
 import Diagrams.Prelude
 import Diagrams.Backend.SVG
 
-import Text.Blaze.Svg.Renderer.Text (renderSvg)
-
 getCircleR :: Handler TypedContent
-getCircleR =
-  let picture = circle 20 # lw 1 # scale 2 # pad 1.1
-  in return $ TypedContent typeSvg $ toContent $ renderSvg $ renderDia SVG (SVGOptions Absolute) (picture :: QDiagram SVG R2 Any)
+getCircleR
+  = return
+  $ TypedContent typeSvg
+  $ toContent
+  $ renderDia SVG (SVGOptions Absolute Nothing)
+  $ (picture :: QDiagram SVG R2 Any)
+  where picture = circle 20 # lw 1 # scale 2 # pad 1.1
