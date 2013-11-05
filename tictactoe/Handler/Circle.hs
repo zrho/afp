49096f70c38aaa5fde2ab197f7685bb03d6dcd62
Lennart Buhl <git@hackmate.de>
2013-11-05 04:13:47 +0100

    diagrams 0.7

--- tictactoe/Handler/Circle.hs
+++ tictactoe/Handler/Circle.hs
@@ -10,6 +10,6 @@ getCircleR
   = return
   $ TypedContent typeSvg
   $ toContent
-  $ renderDia SVG (SVGOptions Absolute Nothing)
+  $ renderDia SVG (SVGOptions Absolute) -- SVGOptions takes 1 arg in 0.7
   $ (picture :: QDiagram SVG R2 Any)
   where picture = circle 20 # lw 1 # scale 2 # pad 1.1
