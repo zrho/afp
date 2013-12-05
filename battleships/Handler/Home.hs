43623865e5b81f0b2f05b3c26009b43f5df0adcb
Meike Grewing <meike@ce-tec.de>
2013-12-05 09:21:27 +0100

    Added empty battleship project, just to get started... ;-)

---
+++ battleships/Handler/Home.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, QuasiQuotes #-}
+module Handler.Home (getHomeR) where
+
+import Import
+import Data.Text
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+             setTitle "Battleships"
---
+++ battleships/Import.hs
@@ -0,0 +1,18 @@
+module Import
+    ( module Import
+    ) where
+
+import           Prelude              as Import hiding (head, init, last,
+                                                 readFile, tail, writeFile)
+import           Yesod                as Import hiding (Route (..))
+
+import           Control.Applicative  as Import (pure, (<$>), (<*>))
+import           Data.Text            as Import (Text)
+
+import           Foundation           as Import
+import           Settings             as Import
+import           Settings.Development as Import
+
+import           Data.Monoid          as Import
+                                                 (Monoid (mappend, mempty, mconcat),
+                                                 (<>))
