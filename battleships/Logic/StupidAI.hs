0a40221dee5f542217e0ccc1e6546496b89aae5a
Fabian Zaiser <fabian.zaiser@googlemail.com>
2014-01-02 18:56:41 +0100

    Rewrite AI ship placement (including bug fix)
    There was a bug in the AI ship placement code: If the first few ships had been
    placed in an unfortunate way, the function failed to terminate because there
    was no allowed way to finish the fleet. I have now implemented backtracking to
    fix this problem.

--- battleships/Logic/StupidAI.hs
+++ battleships/Logic/StupidAI.hs
@@ -22,18 +22,49 @@ instance Serialize StupidAI where
   put StupidAI {..} = S.put rules
 
 initShips :: MonadRandom m => Rules -> m Fleet
-initShips r = foldM f [] $ rulesShips r where
-  f fleet len = liftM2 (:) (placeShip len r fleet) (return fleet)
+initShips r = do
+  result <- initShips' r [] (rulesShips r)
+  case result of
+    Nothing -> undefined -- the grid is too small to place the fleet, there is no valid one
+    Just fleet -> return fleet
 
-placeShip :: MonadRandom m => Int -> Rules -> Fleet -> m Ship
-placeShip len r f = do
-  ship <- getRandomShip (rulesSize r) len
-  case shipAdmissible r f ship of
-    True  -> return ship
-    False -> placeShip len r f
+-- | returns a random fleet, if one exists
+initShips' :: MonadRandom m => Rules
+                            -> Fleet
+                            -> [Int] -- the sizes of the remaining ships to be placed
+                            -> m (Maybe Fleet)
+initShips' r fleet []   = return . Just $ fleet
+initShips' r fleet (len:lens) =
+  let placements = admissibleShipPlacements r fleet len
+  in tryPlacement placements where
+    -- | Given a list of placements for the current ship,
+    -- | try all of them in random order and choose the first one that works out.
+    -- | Place the remaining fleet recursively.
+    tryPlacement :: (MonadRandom m) => [(Pos, Orientation)] -> m (Maybe Fleet)
+    tryPlacement []         = return Nothing -- No feasible placement for the current ship exists,
+                                             -- so there is none for the whole fleet, either.
+    tryPlacement placements = do
+      index <- getRandomR (0, length placements - 1)
+      let (pos, orient) = placements !! index
+          newShip       = Ship pos len orient
+      remainingShips <- initShips' r (newShip : fleet) lens -- place the remaining ships recursively
+      case remainingShips of
+        Just f -> return $ Just f
+        Nothing    -> tryPlacement (removeNth index placements) -- this placement doesn't work, try the next one
+
+-- | calculates all possible placements (Pos, Orientation) for a ship of the given lengths
+admissibleShipPlacements :: Rules -> Fleet -> Int -> [(Pos, Orientation)]
+admissibleShipPlacements r fleet len = filter admissible allPlacements where
+  admissible (pos, orient) = shipAdmissible r fleet $ Ship pos len orient
+  (width, height) = rulesSize r
+  allPlacements = [((x,y), orient) | x <- [0..width-1]
+                                   , y <- [0..height-1]
+                                   , orient <- [Horizontal, Vertical]
+                                   ]
 
 getRandomPos :: MonadRandom m => (Int, Int) -> m (Int,Int)
 getRandomPos (w,h) = liftM2 (,) (getRandomR (0, w - 1)) (getRandomR (0, h - 1))
 
-getRandomShip :: MonadRandom m => (Int, Int) -> Int -> m Ship
-getRandomShip size len = liftM3 Ship (getRandomPos size) (return len) getRandom
+-- can't find this function in the standard libraries...
+removeNth :: Int -> [a] -> [a]
+removeNth n xs = let (ys,zs) = splitAt n xs in ys ++ (tail zs)
