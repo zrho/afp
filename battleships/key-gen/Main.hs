b47ff050ae9db9868e6385d3d2ac0671fe0ea593
Fabian Thorand <f.thorand@gmail.com>
2014-03-30 21:05:30 +0200

    added key generator (see #125)

---
+++ battleships/key-gen/Main.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE PackageImports, RecordWildCards #-}
+module Main where
+import Crypto.Random
+import System.Environment
+import qualified Data.ByteString as BS
+import System.IO
+
+main :: IO ()
+main = do
+  args <- getArgs
+  entPool <- createEntropyPool
+  let
+    cprg    = cprgCreate entPool :: SystemRNG
+    (key,_) = cprgGenerate 32 cprg
+  withFile (head (args ++ ["config/key.aes"])) WriteMode $ \handle -> do
+    BS.hPutStr handle key
+    hFlush handle
+  putStrLn "key generated"
